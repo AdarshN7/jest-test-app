8fcfdc20efc047e56f85c1de893889d4e74e7d43
adarshnishad <adarsh.nishad@nagarro.com>
2023-06-25 17:59:46 +0530

    initial commit

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import Home from "@/pages/index";
+
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Home />);
+
+    const heading = screen.getByRole("heading", {
+      name: /welcome to next\.js!/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
+
+describe("Home", () => {
+  it("renders a sub heading", () => {
+    render(<Home />);
+    expect(screen.getByText("Get started by editing")).toBeInTheDocument;
+  });
+
+  it("should have a button with text click me", () => {
+    render(<Home/>)
+    expect(screen.getByRole('button', {name : 'click me'})).toBeInTheDocument;
+  });
+});
